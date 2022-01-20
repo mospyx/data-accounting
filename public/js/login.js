54f89d4d981b2aca820e0834ffd54978048622e7
Maxim <mospy@yandex.ru>
2022-01-21 00:30:56 +0300

    added axios login.js deleted unused icons

---
+++ public/js/login.js
@@ -0,0 +1,30 @@
+const homePage = 'http://localhost:5000/public/index.html'
+
+let FormS = document.querySelector(".form-data");
+
+FormS.addEventListener('submit', function(e) {
+
+    e.preventDefault();
+
+    let elem = e.target
+
+    let formData = {
+        email: elem.querySelector('[name="email"]').value,
+        password: elem.querySelector('[name="password"]').value,
+    }
+
+    axios.post('http://localhost:5000/api/auth/login', {
+        email: formData.email,
+        password: formData.password,
+    })
+        .then(function(response) {
+            console.log(response.data);
+            let token = response.data.token;
+            localStorage.setItem("jwt", 'Bearer ' + token);
+            axios.defaults.headers.common['Authorization'] = 'Bearer ' + token;
+            if (response.data.code === 200) {
+                window.location.replace(homePage)
+            }
+        })
+
+});
